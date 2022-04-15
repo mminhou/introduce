98e8380b5e3cc604b202b2a39900bc28b2eceda7
mminhou <exit19093@gmail.com>
2022-04-15 17:48:23 +0900

    Feat: 코드 스타일 수정

--- src/component/three/Light.tsx
+++ src/component/three/Light.tsx
@@ -1,18 +1,13 @@
-import React, {useRef} from "react";
+import React, { useRef } from 'react';
 
-export default function Light() {
+const Light = () => {
   const ref = useRef();
   return (
     <>
-      <ambientLight intensity={0.6}/>
-      <directionalLight
-        ref={ref}
-        intensity={0.5}
-        position={[5, 10, 7.5]}
-        shadow-mapSize-width={2048}
-        shadow-mapSize-height={2048}
-        castShadow
-      />
+      <ambientLight intensity={0.6} />
+      <directionalLight ref={ref} intensity={0.5} position={[5, 10, 7.5]} shadow-mapSize-width={2048} shadow-mapSize-height={2048} castShadow />
     </>
-  )
-}
+  );
+};
+
+export default Light;
--- src/component/three/Loader.tsx
+++ src/component/three/Loader.tsx
@@ -1,8 +1,13 @@
-import {Html, useProgress} from "@react-three/drei";
-import React from "react";
+import { Html, useProgress } from '@react-three/drei';
+import React from 'react';
 
-export default function Loader() {
-  const {active, progress, errors, item, loaded, total} = useProgress();
-  return <Html center>{progress} % loaded</Html>;
+const Loader = () => {
+  const { active, progress, errors, item, loaded, total } = useProgress();
+  return (
+    <Html center>
+      {progress} % ${loaded}
+    </Html>
+  );
 };
 
+export default Loader;
