8247e5107109df0cce234a50c7e34611fa2b3ae7
mminhou <exit19093@gmail.com>
2022-04-15 17:47:24 +0900

    Feat: three canvas container 코드 스타일 수정 및 Text3d component 추가

--- src/component/three/CanvasContainer.tsx
+++ src/component/three/CanvasContainer.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import {Canvas} from "@react-three/fiber";
-import {OrbitControls} from "@react-three/drei";
-import {Suspense} from "react";
-import Light from "./Light";
-import Loader from "./Loader";
-import Model from "./Model";
-import Model2 from "./Model2";
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Suspense } from 'react';
+import Light from './Light';
+import Loader from './Loader';
+import Text3d from './Text';
 
 export default function CanvasContainer() {
   return (
     <Canvas>
-      <Suspense fallback={<Loader/>}>
-        <OrbitControls/>
+      <Suspense fallback={<Loader />}>
+        <OrbitControls />
         <Light />
         {/*<Model />*/}
-        <Model2 />
+        {/* <Model2 /> */}
+        <Text3d />
       </Suspense>
     </Canvas>
   );
