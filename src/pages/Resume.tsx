ce3c5e5c05d7eca71aed8bc3c2aa8bde974bd752
mminhou <exit19093@gmail.com>
2022-04-26 17:24:27 +0900

    Feat: Resume grid layout 구성 및 contents 업데이트 ...ing

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 
 const useStyle = makeStyles((theme) => ({
-  
+  mainBackground: {
+    width: '50%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+    marginTop: '30px',
+  },
 }));
 
 const Resume = () => {
@@ -10,7 +17,35 @@ const Resume = () => {
 
   return (
     <div className={classes.mainBackground}>
-      <p>Resume page</p>
+      <Grid container justifyContent='center' alignItems='center' spacing={3}>
+        <Grid item md={4} xs={12} style={{ textAlign: 'left' }}>
+          <h1 style={{ fontSize: '70px' }}>Minho Choi</h1>
+          <h2>Front-End Developer</h2>
+        </Grid>
+        <Grid item md={4}></Grid>
+        <Grid item md={4} xs={12} style={{ textAlign: 'left' }}>
+          <h2>Contact.</h2>
+          <p>exit19093@gmail.com</p>
+          <p>+82)10-2895-1359</p>
+          <h2>Channel.</h2>
+          <p>https://github.com/mminhou</p>
+        </Grid>
+        <Grid item xs={12} style={{ textAlign: 'left' }}>
+          <h2>Introduce</h2>
+          <p>Experienced and passionated Front-end Developer with about one years of experience developing easy to use and fast web page.</p>
+          <p>Reliable developer with the ability to manage and achieve project goals, leveraging my knowledges of Javascript and browsers.</p>
+          <p>Brining proven track record of improving performance, developing well structured web page with good UX, Writing reliable codes with TDD.</p>
+        </Grid>
+        <Grid item md={6} xs={12} style={{ textAlign: 'left' }}>
+          <h1>Education</h1>
+        </Grid>
+        <Grid item md={6} xs={12} style={{ textAlign: 'left' }}>
+          <h1>open source</h1>
+        </Grid>
+        <Grid item xs={12}>
+          <h1>Experience</h1>
+        </Grid>
+      </Grid>
     </div>
   );
 };
