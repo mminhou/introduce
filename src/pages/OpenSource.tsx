b2c74c08ac0caefd6961257385dd7a83c7bfeea7
R _ R <exit19093@gmail.com>
2022-05-10 22:51:44 +0900

    Feat: openSource page template 완성

--- src/pages/OpenSource.tsx
+++ src/pages/OpenSource.tsx
@@ -1,17 +1,73 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { Avatar, Card, CardActions, CardContent, CardHeader, Divider, Grid, IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Star } from '@material-ui/icons';
+import profile from '../assets/images/profile.jpg';
 
 const useStyle = makeStyles((theme) => ({
-  
+  mainBackground: {
+    paddingTop: '20px',
+    display: 'flex',
+  },
+  cardContainer: {
+    minWidth: '350px',
+    textAlign: 'left',
+  },
+  avatar: {
+    border: '0.1px solid lightgray',
+  },
+  mainContent: {
+    textAlign: 'center',
+  },
+  actionContent: {
+    paddingLeft: '15px',
+  },
+  actionStar: {
+    marginRight: '20px',
+  },
+  actionTitle: {
+    color: '#717171',
+  },
+  divider: {
+    width: '95%',
+    margin: 'auto',
+  },
 }));
 
 const OpenSource = () => {
   const classes = useStyle();
 
   return (
-    <div>
-      <p>OpenSource page</p>
-    </div>
+    <Grid container className={classes.mainBackground} direction='column' alignItems='center' spacing={2}>
+      <Grid item xs={12}>
+        <Card className={classes.cardContainer}>
+          <CardHeader
+            avatar={<Avatar src={profile} className={classes.avatar} />}
+            title='web_pen_sdk'
+            subheader='Created By mminhou.'
+            action={
+              <IconButton>
+                <Star />
+              </IconButton>
+            }
+          />
+          <Divider className={classes.divider} />
+          <CardContent className={classes.mainContent}>
+            <Typography variant='body2'>web_pen_sdk for NeoSmartPen.</Typography>
+          </CardContent>
+          <Divider className={classes.divider} />
+          <CardActions className={classes.actionContent}>
+            <Typography variant='body2' className={classes.actionStar}>
+              0 <br />
+              <span className={classes.actionTitle}>STARS</span>
+            </Typography>
+            <Typography variant='body2'>
+              0 <br />
+              <span className={classes.actionTitle}>FORKS</span>
+            </Typography>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
