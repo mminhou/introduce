16fd6501c696f89a7fa740c71d117f2a330298f0
mminhou <exit19093@gmail.com>
2022-05-27 17:13:32 +0900

    Feat: PortfolioDetail component 수정작업중...

--- src/pages/PortfolioDetail.tsx
+++ src/pages/PortfolioDetail.tsx
@@ -1,22 +1,50 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, Typography } from '@material-ui/core';
+import { useParams } from 'react-router-dom';
+import ko from '../util/textData';
 
 const useStyle = makeStyles(() => ({
   mainBackground: {
-    width: '100%',
+    width: '80%',
     height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    margin: '30px auto',
+  },
+  title: {
+    margin: '30px',
   },
 }));
 
 const PortfolioDetail = () => {
   const classes = useStyle();
+  const { companyId, projectId } = useParams();
+
+  let data: any;
+  if (companyId === 'personal') {
+    data = ko.personal.filter((project) => project.id === projectId)[0];
+  } else {
+    data = ko.company.filter((com) => com.id === companyId)[0].project.filter((pro) => pro.id === projectId)[0];
+  }
 
   return (
-    <div>
-      <p>Portfolio Detail page</p>
-    </div>
+    <Grid container className={classes.mainBackground} direction='row' alignItems='center' justifyContent='center'>
+      <Grid item container md={6} xs={12} direction='column' spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant='h4'>{data.title}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='body2'>{data.period}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='body2'>{data.skills}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='body2'>{data.captions}</Typography>
+        </Grid>
+      </Grid>
+      <Grid container item md={6} xs={12} direction='column'>
+        <img src={data.images[0]} width='100%' />
+      </Grid>
+    </Grid>
   );
 };
 
